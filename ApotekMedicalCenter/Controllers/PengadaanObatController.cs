7367ad3cb9d1c1819479ff7e158d057c9d600e8a
anggaeka333 <esteralkohl@yahoo.com>
2013-06-13 23:20:23 +0700

    add views and controller pengadaan obat

---
+++ ApotekMedicalCenter/Controllers/PengadaanObatController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using ApotekMedicalCenter.Models;
+
+
+namespace ApotekMedicalCenter.Controllers
+{
+    class PengadaanObatController
+    {
+    }
+}
---
+++ ApotekMedicalCenter/Views/pengadaan_obat.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ApotekMedicalCenter.Controllers;
+
+namespace ApotekMedicalCenter.Views
+{
+    /// <summary>
+    /// Interaction logic for dataobat.xaml
+    /// </summary>
+    public partial class dataobat : Window
+    {
+        public dataobat()
+        {
+            InitializeComponent();
+
+        }
+
+        private bool IsValid()
+        {
+            bool validation = true;
+
+            if (TextBoxIDObat.Text == "")
+            {
+                TextBlock.Text += "- ID Obat wajib diisi untuk memasukkan ke daftar pembelian.\n";
+                validation = false;
+            }
+
+            return validation;
+        }
+
+        private void ButtonEntry_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock.Text = "";
+
+            if (!IsValid())
+            {
+                return;
+            }
+        }
+
+    }
+}
